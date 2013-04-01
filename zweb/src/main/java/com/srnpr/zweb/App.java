7ee20fd64bdee2a50d3e39a36f4195327e186ee2
liudongpu <liudongpu@hotmail.com>
2013-04-01 21:18:31 +0800

    init

---
+++ zweb/src/main/java/com/srnpr/zweb/App.java
@@ -0,0 +1,13 @@
+package com.srnpr.zweb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
