89ee583f9659555b170453db91ee48bfaedfa649
liudongpu <liudongpu@hotmail.com>
2013-05-22 23:33:18 +0800

    yinfo

---
+++ yinfo/src/main/java/com/srnpr/yinfo/init/InitYinfo.java
@@ -0,0 +1,13 @@
+package com.srnpr.yinfo.init;
+
+import com.srnpr.zcom.base.BaseInit;
+import com.srnpr.zcom.i.IBaseInit;
+
+public class InitYinfo extends BaseInit implements IBaseInit {
+
+	public void Init() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
