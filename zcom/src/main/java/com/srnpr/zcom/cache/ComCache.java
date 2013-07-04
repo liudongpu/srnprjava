5d492c41cf664df1245f9c936ef27de5e7a87325
Administrator <Administrator@PC-201306261119>
2013-07-04 18:00:42 +0800

    a

--- zcom/src/main/java/com/srnpr/zcom/cache/ComCache.java
+++ zcom/src/main/java/com/srnpr/zcom/cache/ComCache.java
@@ -1,8 +1,12 @@
 package com.srnpr.zcom.cache;
 
+import com.srnpr.zcom.manager.ConfigCacheManager;
+
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
 import net.sf.ehcache.Element;
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.config.Configuration;
 
 public class ComCache {
 
@@ -10,13 +14,17 @@ public class ComCache {
 	
 	public void init()
 	{
+		
+		
 		CacheManager singletonManager = CacheManager.create();  
-        Cache memoryOnlyCache = new Cache("testCache", 5000, false, false, 5, 2);  
-        
-       // Element ec=new Element("aa", "bb"); 
-       // memoryOnlyCache.put(ec);
-       
+        Cache memoryOnlyCache = new Cache("testCache", 5000, false, true, 0, 0);  
         singletonManager.addCache(memoryOnlyCache);  
+        
+        
+        Element ec=new Element("aa", "bb"); 
+        memoryOnlyCache.put(ec);
+       
+        //WsingletonManager.addCache(memoryOnlyCache);  
         Cache testCache = singletonManager.getCache("testCache");  
         
         
