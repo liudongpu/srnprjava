a59b5b2793f50302b637fadb86c9b73a89d59ce6
srnpr <srnprhomemacmini@srnpr.com>
2013-07-25 23:32:20 +0800

    1

---
+++ zzero/src/main/java/com/srnpr/zzero/web/zen/zen_ahinfo.js
@@ -0,0 +1,15 @@
+zen
+		.e({
+			ahinfo : {
+				init : function() {
+					$('.b_corner').corner();
+					//zen.ahinfo.checklogin();
+
+				}
+
+			}
+		});
+
+$(window).ready(function() {
+	zen.ahinfo.init();
+});
